a6ed0d5a9572485c2030c4be845bcf15b345696d
EC2 Default User <ec2-user@ip-10-88-170-115.ap-southeast-2.compute.internal>
2018-03-19 08:58:00 +0000

    .gitignore file - ignore csv and zip data files

---
+++ derby_implementation.java
@@ -0,0 +1,8 @@
+/*Task 1: Derby
+ * You are required to load the data into Derby. In your report:
+ * explain how have you chosen to structure the Derby relational database and give reasons.
+ * provide details of the time to load the data into Derby. You need to analyse the data and consider appropriate ways to 
+ * structure the data and then using any scripting, programming or other tools to format the data accordingly.
+ * Postgraduate students only: What alternative way or ways could you have organised the data when storing in Derby, and what advantages 
+ * or disadvantages would these al- ternative designs have? 
+*/
---
+++ mongo_implementation.java
@@ -0,0 +1,9 @@
+/*Task 2: MongoDB
+ * You are required to load the data into MongoDB. In your report:
+ * explain how have you chosen to structure the data inserted in MongoDB
+ * provide details of the time taken to load the data (The mongoimport is one utility will provide such information). Please note that 
+ * a naive import into a flat structure in Mongodb will not accrue you a great mark. You need to analyse the data and consider 
+ * appropriate ways to structure the data and then using any scripting, programming or other tools to format the data accordingly.
+ * Postgraduate students only: What alternative way or ways could you have organised the data when storing in MongoDB, 
+ * and what advantages or disadvantages would these alternative designs have? 
+*/
